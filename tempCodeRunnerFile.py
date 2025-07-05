a114030c5a4a0b06d0af1bfb4b3914347ca5b245
Aniket-IITP <aniketgiri.0723@gmail.com>
2025-07-05 22:28:21 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+   # img_top1=Image.open(r"D:\final project\ATTENDANC3E USING FACE RECOGNITION\college pics\middle image.jpg")
+        # img_top1=img_top1.resize((200,200),Image.Resampling.LANCZOS)
+        # self.photoimg_top1=ImageTk.PhotoImage(img_top1)
+
+        # f_lbl=Label(main_frame, image=self.photoimg_top1)
+        # f_lbl.place(x=300, y=0, width=200, height=200)
